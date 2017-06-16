a4dd719ee4a067680c8ec899767ebb6a52242b3f
AbhiShubh <bini.abhi@gmail.com>
2017-06-16 17:32:22 +0530

    Add files via upload

---
+++ AnalyticsBaseExceptions.java
@@ -0,0 +1,38 @@
+package com.analytics.exceptions;
+
+public class AnalyticsBaseExceptions  extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public AnalyticsBaseExceptions() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AnalyticsBaseExceptions(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AnalyticsBaseExceptions(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AnalyticsBaseExceptions(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AnalyticsBaseExceptions(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	/*public IAnalyticsBaseException(String message , Analysis analysis){
+		
+		super (message,analysis);
+		
+	}*/
+
+}
